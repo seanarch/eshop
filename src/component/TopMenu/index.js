ed5edaad2b446f42d30489b6d612b93a8693fb18
Sean <seanzzz@Seans-Mac-Studio.local>
2023-03-12 16:36:22 -0600

    init

---
+++ src/component/TopMenu/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./_topmenu.scss";
+
+const TopMenu = () => {
+  const menu = ["Home", "Women", "Men", "Kids", "Best Seller"];
+  return (
+    <>
+      <header className="menuHeader">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xl-12 col-lg-12">
+              <nav className="menu">
+                <ul>
+                  {menu.map((item, index) => (
+                    <li key={index}>
+                      <a href="#">{item}</a>
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default TopMenu;
